284886a5fdfb679933cc01614b81f72d3dc8cebd
0rangeBlaze <gardevdaniel@gmail.com>
2023-09-18 10:09:07 +0200

    dotnet version 7

--- WorldOfZuul/obj/Debug/net7.0/WorldOfZuul.AssemblyInfo.cs
+++ WorldOfZuul/obj/Debug/net7.0/WorldOfZuul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a737e15b5718201b47867777c0ec4067befe8868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
