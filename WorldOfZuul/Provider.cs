ee8d26970ceab02b1821c04f218ab31d48a538ab
InFamous11 <nieslawnyy1@gmail.com>
2023-10-30 10:39:25 +0100

    Implemented Supply Cholice with supply Providers

---
+++ WorldOfZuul/Provider.cs
@@ -0,0 +1,15 @@
+namespace WorldOfZuul
+{
+    public class Provider
+    {
+        public string? ProviderName {get; set;}
+        public int personalWelfareChange {get; set;}
+        public int populationWelfareChange {get; set;}
+        public int environmentChange {get; set;}
+        public string? providerDescription {get; set;}
+    }
+    public class Food
+    {
+        public string? FoodName {get; set;}
+    }
+}
