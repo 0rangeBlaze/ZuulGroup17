a310d5cd47c35360aad966c9f77a4611e79e64e6
Zuhao <yalin1998@hotmail.com>
2023-10-31 10:39:06 +0100

    Added invalid input function and a dictionary for storing trait stats.

--- WorldOfZuul/Game.cs
+++ WorldOfZuul/Game.cs
@@ -54,28 +54,38 @@ namespace WorldOfZuul
             Console.WriteLine();
         }
 
-        
 
-        
+
+
         private void Hire()
         {
             List<string> hireNames = new List<string>
-            {
-                "Zuhao", "Oskar", "Daniel", "SebestyÃ©n", "Szymon"
-            };
+    {
+        "Zuhao", "Oskar", "Daniel", "Sebestyén", "Szymon"
+    };
 
             List<string> hireHobbies = new List<string>
-            {
-                "Filler1", "Filler2", "Filler3", "Filler4", "Filler5"
-            };
+    {
+        "Filler1", "Filler2", "Filler3", "Filler4", "Filler5"
+    };
 
             List<string> hireLastJobs = new List<string>
-            {
-                "Filler1", "Filler2", "Filler3", "Filler4", "Filler5"
-            };
+    {
+        "Filler1", "Filler2", "Filler3", "Filler4", "Filler5"
+    };
+
+            Dictionary<string, int> traitValues = new Dictionary<string, int>
+    {
+        { "Filler1", 1 },   // Stat changes for different traits (can be negative?)
+        { "Filler2", 2 },
+        { "Filler3", 3 },
+        { "Filler4", 4 },
+        { "Filler5", 5 },
+    };
+
             int hires = 0;
 
-            while (hires < 5)
+            while (hires < 5) // Loop until the player has hired five times
             {
                 string[] hireTraits = GetRandomCandidate(hireNames, hireHobbies, hireLastJobs);
                 string hireName = hireTraits[0];
@@ -86,16 +96,39 @@ namespace WorldOfZuul
                 Console.WriteLine($"Hobbies: {hireHobby}");
                 Console.WriteLine($"Last Job: {hireLastJob}");
 
-                Console.Write("Do you want to hire this candidate? (Yes/No): ");
-                string decision = Console.ReadLine();
+                string decision = string.Empty;
+                bool validDecision = false;
 
-                if (decision == "Yes")
+                while (!validDecision)
                 {
-                    // stat changes based on randomized traits
-                    hires++;
+                    Console.Write("Do you want to hire this candidate? (Yes/No): ");
+                    decision = Console.ReadLine();
+
+                    if (decision.Equals("Yes"))
+                    {
+                        // Stat changes based on randomized traits
+                        int hobbyValue = traitValues[hireHobby];
+                        int lastJobValue = traitValues[hireLastJob];
+
+                        // Update game stats based on trait values
+                        //Examples:
+                        //personalWelfare += hobbyValue;
+                        //populationWelfare += lastJobValue;
+                        //something for environment +=
+
+                        hires++;
+                        validDecision = true;
+                    }
+                    else if (decision.Equals("No"))
+                    {
+                        validDecision = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter 'Yes' or 'No'.");
+                    }
                 }
             }
-
         }
 
         private static void SupplyChoice()
