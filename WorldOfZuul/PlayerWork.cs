ebe378eb45785afae8519fb6acd69cee66459df1
0rangeBlaze <gardevdaniel@gmail.com>
2023-11-25 19:37:22 +0100

    fixing things

--- WorldOfZuul/PlayerWork.cs
+++ WorldOfZuul/PlayerWork.cs
@@ -199,8 +199,9 @@ namespace WorldOfZuul
 
                     // Update game stats based on trait values
                     //Examples:
-                    //game.World.PersonalWelfare += hobbyPersonal + lastJobPersonal;
-                    //game.World.Environment += hobbyEnvironment + lastJobEnvironment;
+
+                    personalWelfare += hobbyPersonal + lastJobPersonal;
+                    game.World.Environment += hobbyEnvironment + lastJobEnvironment;
                     hires++;
                 } 
             
