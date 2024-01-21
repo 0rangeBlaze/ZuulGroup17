52d271bf8772ddb8ca5cb2edeac7d84abec4a5ad
0rangeBlaze <gardevdaniel@gmail.com>
2024-01-21 12:48:51 +0100

    updateing publish.sh

--- WorldOfZuul/publish.sh
+++ WorldOfZuul/publish.sh
@@ -4,5 +4,5 @@ dotnet publish -o ./app --os win --self-contained true -p:DebugType=None -p:Debu
 mv ./app/WorldOfZuul.exe ./app/WorldOfZuul-win-x64.exe
 dotnet publish -o ./app --os osx --self-contained true -p:DebugType=None -p:DebugSymbols=false
 mv ./app/WorldOfZuul ./app/WorldOfZuul-mac
-cp -r ./assets ./app
+cp -rf ./assets ./app
 zip -r app.zip ./app
